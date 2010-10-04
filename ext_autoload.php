fd78480a2c2044512dfc2d46baa88fb34cbf7b9f
Apen <cerdanyohann@gmail.com>
2010-10-04 22:02:09 +0200

    Initial release

---
+++ ext_autoload.php
@@ -0,0 +1,13 @@
+<?php
+
+$reports = array('eid','clikeys','plugins','xclass');
+$extensionPath = t3lib_extMgm::extPath('additional_reports');
+$autoload = array();
+
+foreach ($reports as $report) {
+	$autoload['tx_additionalreports_'.$report] = $extensionPath . 'reports_'.$report.'/class.tx_additionalreports_'.$report.'.php';
+}
+
+return $autoload;
+
+?>
---
+++ reports_eid/class.tx_additionalreports_eid.php
@@ -0,0 +1,106 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2009-2010 Francois Suter <francois@typo3.org>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+
+/**
+ * This class provides a report displaying a list of all installed services
+ * Code inspired by EXT:dam/lib/class.tx_dam_svlist.php by Rene Fritz
+ *
+ * @author		Francois Suter <francois@typo3.org>
+ * @package		TYPO3
+ * @subpackage	sv
+ *
+ * $Id: class.tx_sv_reports_serviceslist.php 7905 2010-06-13 14:42:33Z ohader $
+ */
+class tx_additionalreports_eid implements tx_reports_Report {
+	/**
+	 * Back-reference to the calling reports module
+	 *
+	 * @var	tx_reports_Module	$reportObject
+	 */
+	protected $reportObject;
+
+	/**
+	 * Constructor for class tx_sv_reports_ServicesList
+	 *
+	 * @param	tx_reports_Module	Back-reference to the calling reports module
+	 */
+	 
+	public function __construct(tx_reports_Module $reportObject) {
+		$this->reportObject = $reportObject;
+		$GLOBALS['LANG']->includeLLFile('EXT:additional_reports/locallang.xml');
+	}
+
+	/**
+	 * This method renders the report
+	 *
+	 * @return	string	The status report as HTML
+	 */
+	 
+	public function getReport() {
+		$content = '';
+
+		// Add custom stylesheet
+		$this->reportObject->doc->getPageRenderer()->addCssFile(t3lib_extMgm::extRelPath('additional_reports') . 'tx_additionalreports.css');
+		$content .= '<p class="help">'.$GLOBALS['LANG']->getLL('eid_description').'</p>';
+		$content .= $this->displayEID();
+
+		return $content;
+	}
+
+	protected function displayEID() {
+		$content = '';
+		$items = $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include'];
+		$content .= '<table cellspacing="1" cellpadding="2" border="0" class="tx_sv_reportlist">';
+		$content .= '<tr><td colspan="7">';
+		$content .= '<h4></h4>';
+		$content .= '</td></tr>';
+		$content .= '<tr class="bgColor2">';
+		$content .= '<td class="cell"></td>';
+		$content .= '<td class="cell">'.$GLOBALS['LANG']->getLL('extension').'</td>';
+		$content .= '<td class="cell">'.$GLOBALS['LANG']->getLL('name').'</td>';
+		$content .= '<td class="cell">'.$GLOBALS['LANG']->getLL('path').'</td>';
+		$content .= '</tr>';
+		foreach ($items as $itemKey => $itemValue) {
+			preg_match('/EXT:(.*?)\//', $itemValue,$ext);
+			$content .= '<tr class="bgColor3-20">';
+			$content .= '<td class="cell" align="center" valign="middle"><img src="' . t3lib_extMgm::extRelPath($ext[1]) . 'ext_icon.gif"/></td>';
+			$content .= '<td class="cell">' . $ext[1] . '</td>';
+			$content .= '<td class="cell">' . $itemKey . '</td>';
+			$content .= '<td class="cell">' . $itemValue . '</td>';
+			$content .= '</tr>';
+		
+		}
+		$content .= '</table>';
+		return $content;
+	}
+
+}
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/additional_reports/reports_eid/class.tx_additionalreports_eid.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/additional_reports/reports_eid/class.tx_additionalreports_eid.php']);
+}
+
+?>
