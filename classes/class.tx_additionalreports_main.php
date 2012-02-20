f03a9531477f2aa20ca31ec9cfda560996376710
Apen <cerdanyohann@yahoo.fr>
2012-02-20 15:33:31 +0100

    Bugfix on eid list

--- classes/class.tx_additionalreports_main.php
+++ classes/class.tx_additionalreports_main.php
@@ -103,12 +103,14 @@ class tx_additionalreports_main
 		$content .= '</tr>';
 		foreach ($items as $itemKey => $itemValue) {
 			preg_match('/EXT:(.*?)\//', $itemValue, $ext);
-			$content .= '<tr class="db_list_normal">';
-			$content .= '<td class="col-icon"><img src="' . t3lib_div::getIndpEnv('TYPO3_REQUEST_DIR') . t3lib_extMgm::extRelPath($ext[1]) . 'ext_icon.gif"/></td>';
-			$content .= '<td class="cell">' . $ext[1] . '</td>';
-			$content .= '<td class="cell">' . $itemKey . '</td>';
-			$content .= '<td class="cell">' . $itemValue . '</td>';
-			$content .= '</tr>';
+			if (t3lib_extMgm::isLoaded($ext[1])) {
+				$content .= '<tr class="db_list_normal">';
+				$content .= '<td class="col-icon"><img src="' . t3lib_div::getIndpEnv('TYPO3_REQUEST_DIR') . t3lib_extMgm::extRelPath($ext[1]) . 'ext_icon.gif"/></td>';
+				$content .= '<td class="cell">' . $ext[1] . '</td>';
+				$content .= '<td class="cell">' . $itemKey . '</td>';
+				$content .= '<td class="cell">' . $itemValue . '</td>';
+				$content .= '</tr>';
+			} 
 		}
 		$content .= '</table>';
 		return $content;
