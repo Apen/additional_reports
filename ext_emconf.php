cac24d23e8da6e959290c7befbc348c921b1db0f
Apen <cerdanyohann@yahoo.fr>
2015-12-07 10:22:12 +0100

    [BUGFIX] Update em_conf

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "additional_reports".
  *
- * Auto generated 06-12-2015 16:12
+ * Auto generated 07-12-2015 11:22
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -15,12 +15,12 @@ $EM_CONF[$_EXTKEY] = array (
 	'description' => 'Useful information in the reports module: xclass, ajax, cliKeys, eID, general status of the system (encoding, DB, php vars...), hooks, compare local and TER extension (diff), used content type, used plugins, ExtDirect... It can really help you during migration or new existing project (to have global reports of the system).',
 	'category' => 'misc',
 	'shy' => true,
-	'version' => '3.0.2',
+	'version' => '3.0.3',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
 	'state' => 'stable',
-	'uploadfolder' => false,
+	'uploadfolder' => true,
 	'createDirs' => '',
 	'modify_tables' => '',
 	'clearcacheonload' => true,
@@ -34,7 +34,6 @@ $EM_CONF[$_EXTKEY] = array (
 	array (
 		'depends' => 
 		array (
-			'PHP' => '5.3.0-5.6.99',
 			'TYPO3' => '6.2.0-7.6.99',
 		),
 		'conflicts' => 
