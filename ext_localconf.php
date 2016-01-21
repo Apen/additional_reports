be9ee5b9f56a3d1f991fe726c560406a254a80f2
Apen <cerdanyohann@gmail.com>
2016-01-21 10:01:40 +0100

    [BUGFIX] Fix ambiguous class warning

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,11 +4,11 @@
 
 if (version_compare(TYPO3_version, '7.6.0', '>=')) {
     $GLOBALS['TYPO3_CONF_VARS']['BE']['AJAX'][$_EXTKEY . '::compareFiles'] = [
-        'callbackMethod' => 'EXT:' . $_EXTKEY . '/Classes/Eid/class.tx_additionalreports_ajax.php:tx_additionalreports_ajax->main',
+        'callbackMethod' => 'EXT:' . $_EXTKEY . '/Classes/Eid/class.tx_additionalreports_callajax.php:tx_additionalreports_callajax->main',
         'csrfTokenCheck' => false
     ];
 } else {
-    $GLOBALS['TYPO3_CONF_VARS']['BE']['AJAX'][$_EXTKEY . '::compareFiles'] = 'EXT:' . $_EXTKEY . '/Classes/Eid/class.tx_additionalreports_ajax.php:tx_additionalreports_ajax->main';
+    $GLOBALS['TYPO3_CONF_VARS']['BE']['AJAX'][$_EXTKEY . '::compareFiles'] = 'EXT:' . $_EXTKEY . '/Classes/Eid/class.tx_additionalreports_callajax.php:tx_additionalreports_callajax->main';
 }
 
 ?>
