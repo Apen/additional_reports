358891dbb3dce07529a2cfce13741b387ab40a16
CERDAN Yohann <cerdanyohann@yahoo.fr>
2019-09-06 11:11:58 +0200

    [BUGFIX] Fix the version dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,39 +1,27 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "additional_reports".
- *
- * Auto generated 21-09-2015 16:17
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
-$EM_CONF[$_EXTKEY] = array (
-	'title' => 'Useful information in reports module',
-	'description' => 'Useful information in the reports module: xclass, ajax, cliKeys, eID, general status of the system (encoding, DB, php vars...), hooks, compare local and TER extension (diff), used content type, used plugins, ExtDirect... It can really help you during migration or new existing project (to have global reports of the system).',
-	'category' => 'misc',
-	'version' => '3.1.4',
-	'state' => 'stable',
-	'uploadfolder' => '', 
-	'createDirs' => '',
-	'clearcacheonload' => true,
-	'author' => 'CERDAN Yohann',
-	'author_email' => 'cerdanyohann@yahoo.fr',
-	'author_company' => '',
-	'constraints' => 
-	array (
-		'depends' => 
-		array (
-			'typo3' => '8.7.0-8.7.99',
-		),
-		'conflicts' => 
-		array (
-		),
-		'suggests' => 
-		array (
-		),
-	),
+$EM_CONF[$_EXTKEY] = array(
+    'title'            => 'Useful information in reports module',
+    'description'      => 'Useful information in the reports module: xclass, ajax, cliKeys, eID, general status of the system (encoding, DB, php vars...), hooks, compare local and TER extension (diff), used content type, used plugins, ExtDirect... It can really help you during migration or new existing project (to have global reports of the system).',
+    'category'         => 'misc',
+    'version'          => '3.1.4',
+    'state'            => 'stable',
+    'uploadfolder'     => '',
+    'createDirs'       => '',
+    'clearcacheonload' => true,
+    'author'           => 'CERDAN Yohann',
+    'author_email'     => 'cerdanyohann@yahoo.fr',
+    'author_company'   => '',
+    'constraints'      =>
+        array(
+            'depends'   =>
+                array(
+                    'typo3' => '8.7.0-9.5.99',
+                ),
+            'conflicts' =>
+                array(),
+            'suggests'  =>
+                array(),
+        ),
 );
 
