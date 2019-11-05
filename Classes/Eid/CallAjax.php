88f40023155686e6452c9f3fbc599f9747cce5b2
CERDAN Yohann <cerdanyohann@yahoo.fr>
2019-11-05 16:03:28 +0100

    [BUFIX] Fix CompareFiles view

--- Classes/Eid/CallAjax.php
+++ Classes/Eid/CallAjax.php
@@ -3,24 +3,18 @@
 namespace Sng\AdditionalReports\Eid;
 
 /*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
+ * This file is part of the "additional_reports" Extension for TYPO3 CMS.
  *
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
  */
 
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class CallAjax
 {
-    public function main()
+    public function main(\Psr\Http\Message\ServerRequestInterface $request, \Psr\Http\Message\ResponseInterface $response)
     {
         include(PATH_site . 'typo3conf/ext/additional_reports/Classes/Eid/class.tx_compareFiles_eID.php');
+        return $response;
     }
 }
