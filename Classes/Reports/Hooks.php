f7fe58b6887e21179b01d201ed2ada7adde11f62
CERDAN Yohann <cerdanyohann@gmail.com>
2019-11-05 11:37:30 +0100

    [TASK] Move code from "Main" class to reporting class

--- Classes/Reports/Hooks.php
+++ Classes/Reports/Hooks.php
@@ -22,8 +22,58 @@ class Hooks extends \Sng\AdditionalReports\Reports\AbstractReport implements \TY
     public function getReport()
     {
         $content = '<p class="help">' . $GLOBALS['LANG']->getLL('hooks_description') . '</p>';
-        $content .= \Sng\AdditionalReports\Main::displayHooks();
+        $content .= $this->display();
         return $content;
+    }
+
+    /**
+     * Generate the hooks report
+     *
+     * @return string HTML code
+     */
+    public function display()
+    {
+        $hooks = array();
+
+        // core hooks
+        $items = $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'];
+        if (count($items) > 0) {
+            foreach ($items as $itemKey => $itemValue) {
+                if (preg_match('/.*?\/.*?\.php/', $itemKey, $matches)) {
+                    foreach ($itemValue as $hookName => $hookList) {
+                        $hookList = \Sng\AdditionalReports\Utility::getHook($hookList);
+                        if (!empty($hookList)) {
+                            $hooks['core'][] = array(
+                                'corefile' => $itemKey,
+                                'name'     => $hookName,
+                                'file'     => \Sng\AdditionalReports\Utility::viewArray($hookList)
+                            );
+                        }
+                    }
+                }
+            }
+        }
+
+        $items = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'];
+        if (count($items) > 0) {
+            foreach ($items as $itemKey => $itemValue) {
+                foreach ($itemValue as $hookName => $hookList) {
+                    $hookList = \Sng\AdditionalReports\Utility::getHook($hookList);
+                    if (!empty($hookList)) {
+                        $hooks['extensions'][] = array(
+                            'corefile' => $itemKey,
+                            'name'     => $hookName,
+                            'file'     => \Sng\AdditionalReports\Utility::viewArray($hookList)
+                        );
+                    }
+                }
+            }
+        }
+
+        $view = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Fluid\\View\\StandaloneView');
+        $view->setTemplatePathAndFilename(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('additional_reports') . 'Resources/Private/Templates/hooks-fluid.html');
+        $view->assign('hooks', $hooks);
+        return $view->render();
     }
 
 }
