c64f9388adbb941e723a5c6f090d81e8b2c5f852
CERDAN Yohann <cerdanyohann@yahoo.fr>
2019-11-05 11:19:23 +0100

    [BUGFIX] Fix call to $_EXTKEY

--- ext_tables.php
+++ ext_tables.php
@@ -7,13 +7,11 @@ if (!defined('TYPO3_MODE')) {
 if (TYPO3_MODE == 'BE') {
     $reports = \Sng\AdditionalReports\Utility::getReportsList();
     foreach ($reports as $report) {
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports'][$_EXTKEY][$report[1]] = array(
-            'title'       => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xlf:' . $report[1] . '_title',
-            'description' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xlf:' . $report[1] . '_description',
-            'icon'        => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/tx_additionalreports_' . $report[1] . '.png',
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['additional_reports'][$report[1]] = array(
+            'title'       => 'LLL:EXT:additional_reports/Resources/Private/Language/locallang.xlf:' . $report[1] . '_title',
+            'description' => 'LLL:EXT:additional_reports/Resources/Private/Language/locallang.xlf:' . $report[1] . '_description',
+            'icon'        => 'EXT:additional_reports/Resources/Public/Icons/tx_additionalreports_' . $report[1] . '.png',
             'report'      => 'Sng\AdditionalReports\Reports\\' . $report[0]
         );
     }
 }
-
-?>
