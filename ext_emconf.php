f0095faec3c6ddfc7ceac1ae4a268d6e7d76728a
Apen <cerdanyohann@gmail.com>
2020-04-30 20:42:34 +0200

    [TASK] Set version to 3.2.3-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,10 +1,10 @@
 <?php
 
-$EM_CONF['additional_reports'] = array(
+$EM_CONF['additional_reports'] = [
     'title'            => 'Useful information in reports module',
     'description'      => 'Useful information in the reports module: xclass, ajax, cliKeys, eID, general status of the system (encoding, DB, php vars...), hooks, compare local and TER extension (diff), used content type, used plugins, ExtDirect... It can really help you during migration or new existing project (to have global reports of the system).',
     'category'         => 'misc',
-    'version'          => '3.2.2',
+    'version'          => '3.2.3-dev',
     'state'            => 'stable',
     'uploadfolder'     => '',
     'createDirs'       => '',
@@ -13,15 +13,15 @@ $EM_CONF['additional_reports'] = array(
     'author_email'     => 'cerdanyohann@yahoo.fr',
     'author_company'   => '',
     'constraints'      =>
-        array(
+        [
             'depends'   =>
-                array(
+                [
                     'typo3' => '8.7.0-9.5.99',
-                ),
+                ],
             'conflicts' =>
-                array(),
+                [],
             'suggests'  =>
-                array(),
-        ),
-);
+                [],
+        ],
+];
 
