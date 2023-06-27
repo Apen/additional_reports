7927db848fcabb256de3688f7f6b4cefe4eee8d3
Apen <cerdanyohann@gmail.com>
2023-06-27 15:45:31 +0200

    [BUGFIX] Fix emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,10 +1,10 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['additional_reports'] = [
     'title' => 'Useful information in reports module',
     'description' => 'Useful information in the reports module: xclass, ajax, cliKeys, eID, general status of the system (encoding, DB, php vars...), hooks, compare local and TER extension (diff), used content type, used plugins, ExtDirect... It can really help you during migration or new existing project (to have global reports of the system).',
     'category' => 'misc',
-    'version' => '3.4.3',
+    'version' => '3.4.4',
     'state' => 'stable',
     'uploadfolder' => '',
     'createDirs' => '',
