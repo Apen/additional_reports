b913ba03297ee00fd1a00dbca18029ad13d88e4f
Apen <cerdanyohann@gmail.com>
2023-07-06 16:35:22 +0200

    [TASK] First patch for v12

--- ext_tables.php
+++ ext_tables.php
@@ -1,17 +1,15 @@
 <?php
 
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
+defined('TYPO3') or die();
 
-if (TYPO3_MODE == 'BE') {
-    $reports = \Sng\AdditionalReports\Utility::getReportsList();
-    foreach ($reports as $report) {
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['additional_reports'][$report[1]] = [
-            'title'       => 'LLL:EXT:additional_reports/Resources/Private/Language/locallang.xlf:' . $report[1] . '_title',
-            'description' => 'LLL:EXT:additional_reports/Resources/Private/Language/locallang.xlf:' . $report[1] . '_description',
-            'icon'        => 'EXT:additional_reports/Resources/Public/Icons/tx_additionalreports_' . $report[1] . '.png',
-            'report'      => 'Sng\AdditionalReports\Reports\\' . $report[0]
-        ];
-    }
+$reports = \Sng\AdditionalReports\Utility::getReportsList();
+
+// only for typo3 < 12
+foreach ($reports as $report) {
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['additional_reports'][$report[1]] = [
+        'title'       => 'LLL:EXT:additional_reports/Resources/Private/Language/locallang.xlf:' . $report[1] . '_title',
+        'description' => 'LLL:EXT:additional_reports/Resources/Private/Language/locallang.xlf:' . $report[1] . '_description',
+        'icon'        => 'EXT:additional_reports/Resources/Public/Icons/tx_additionalreports_' . $report[1] . '.png',
+        'report'      => 'Sng\AdditionalReports\Reports\\' . $report[0]
+    ];
 }
