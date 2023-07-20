60fd69783eb1a43ae3e34a75586c69c4751c6d17
Apen <cerdanyohann@gmail.com>
2023-07-20 11:17:29 +0200

    [TASK] Version 3.4.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,8 +3,7 @@
 $EM_CONF['additional_reports'] = [
     'title' => 'Useful information in reports module',
     'description' => 'Useful information in the reports module: xclass, ajax, cliKeys, eID, general status of the system (encoding, DB, php vars...), hooks, compare local and TER extension (diff), used content type, used plugins, ExtDirect... It can really help you during migration or new existing project (to have global reports of the system).',
-    'category' => 'misc',
-    'version' => '3.4.4',
+    'version' => '3.4.5',
     'state' => 'stable',
     'uploadfolder' => '',
     'createDirs' => '',
@@ -14,7 +13,7 @@ $EM_CONF['additional_reports'] = [
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-12.4.99',
+            'typo3' => '11.5.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
