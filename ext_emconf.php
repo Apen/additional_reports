d73cad5087c65e335eeaa0277e8d2ee68f03bf36
Apen <cerdanyohann@gmail.com>
2026-08-08 09:26:40 +0200

    [BUGFIX] Make TER metadata extractable

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,11 @@
 <?php
 
-declare(strict_types=1);
-
-$EM_CONF['additional_reports'] = [
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Useful information in reports module',
     'description' => 'Useful information in the reports module: xclass, ajax, cliKeys, eID, general status of the system (encoding, DB, php vars...), hooks, compare local and TER extension (diff), used content type, used plugins, ExtDirect... It can really help you during migration or new existing project (to have global reports of the system).',
     'version' => '4.0.0',
     'state' => 'stable',
-    'clearcacheonload' => true,
+    'clearCacheOnLoad' => true,
     'author' => 'CERDAN Yohann',
     'author_email' => 'cerdanyohann@yahoo.fr',
     'author_company' => '',
